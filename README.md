a8e6ee29cf8a337afc08ce398b8594772aad0995
Cheng Gang <chenggangpro@gmail.com>
2022-01-08 17:48:27 +0800

    guess game

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ rust-playground with rustlings
 playground with rustlings
 
 #####`guessing_game`
-* [ ] https://doc.rust-lang.org/book/print.html#storing-values-with-variables
+* [x] https://doc.rust-lang.org/book/print.html#storing-values-with-variables
+* [ ] https://doc.rust-lang.org/book/print.html#common-programming-concepts
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,41 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(0..10);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Please type a number! {}",err.to_string());
+                break;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        };
+
+    }
 }
