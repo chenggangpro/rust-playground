62d25355bba0d4f90645ac4282146b3f83d9bea4
Cheng Gang <chenggangpro@gmail.com>
2022-01-11 13:36:10 +0800

    variables

--- new_variables/src/main.rs
+++ new_variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // let mut
     let mut x = 5;
@@ -15,4 +17,33 @@ fn main() {
         println!("The value of x in the inner scope is: {}", x);
     }
     println!("The value of x is: {}", x);
+    //parse string to number
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    println!("Parsed value:{}",guess);
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    if index > a.len() -1 {
+        println!("Out of index :{}",index);
+        return;
+    }
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
